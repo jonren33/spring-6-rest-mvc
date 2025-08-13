9507bc774164d8c0ade4190d50c4e0bc4cb6ab9c
Jonathan Reynolds <jonathan.reynolds@gmail.com>
2025-08-13 04:33:15 -0400

    Update BeerStyle now works

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -66,6 +66,7 @@ public class BeerServiceImpl implements BeerService {
     public void updateBeerById(UUID beerId, Beer beer) {
         Beer existing = beerMap.get(beerId);
         existing.setBeerName(beer.getBeerName());
+        existing.setBeerStyle(beer.getBeerStyle());
         existing.setPrice(beer.getPrice());
         existing.setUpc(beer.getUpc());
         existing.setQuantityOnHand(beer.getQuantityOnHand());
