1caaf1c2000fe01d61cad39884b5ddcf693cec28
Jonathan Reynolds <jonathan.reynolds@gmail.com>
2025-08-14 18:26:14 -0400

    Pulled in UpdateCustomerById Test

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -14,12 +14,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -84,5 +85,23 @@ class CustomerControllerTest {
                         .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void testUpdateCustomerById() throws Exception {
+
+        Customer customer = customerServiceImpl.listCustomers().getFirst();
+
+        given(customerService.updateCustomer(any(UUID.class), any(Customer.class)))
+                .willReturn(customerServiceImpl.listCustomers().getFirst());
+
+        mockMvc.perform(put("/api/v1/customer/" + customer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(status().isNoContent())
+                .andExpect(header().exists("Location"));
+
+        verify(customerService).updateCustomer(any(UUID.class), any(Customer.class));
     }
 }
