495673f7340988b17c06be1d60f3ffc3280652b9
Jonathan Reynolds <jonathan.reynolds@gmail.com>
2025-08-25 08:45:11 -0400

    PUlled in John's changes for paging the getBeersByXXX

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
@@ -13,11 +15,11 @@ import java.util.UUID;
  */
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
-    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
+    Page<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName, Pageable pageable);
 
-    List<Beer> findAllByBeerStyle(BeerStyle beerStyle);
+    Page<Beer> findAllByBeerStyle(BeerStyle beerStyle, Pageable pageable);
 
-    List<Beer> findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle(String beerName, BeerStyle beerStyle);
+    Page<Beer> findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle(String beerName, BeerStyle beerStyle, Pageable pageable);
 
     //These last two aren't used.  I was just fooling around with what is capable with JPA
     List<Beer> findAllByBeerNameIsLikeIgnoreCaseAndUpcIsLikeIgnoreCaseAndBeerStyleAndPriceGreaterThan(String beerName, String upc, BeerStyle beerStyle, BigDecimal price);
