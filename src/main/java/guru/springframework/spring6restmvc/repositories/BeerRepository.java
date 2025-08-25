df115ccef5f5cc63610655daf88c7cf52b36c2bb
Jonathan Reynolds <jonathan.reynolds@gmail.com>
2025-08-25 01:40:12 -0400

    Added two repository queries and tests just to see what can be done. Added comment

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -19,6 +19,7 @@ public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     List<Beer> findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle(String beerName, BeerStyle beerStyle);
 
+    //These last two aren't used.  I was just fooling around with what is capable with JPA
     List<Beer> findAllByBeerNameIsLikeIgnoreCaseAndUpcIsLikeIgnoreCaseAndBeerStyleAndPriceGreaterThan(String beerName, String upc, BeerStyle beerStyle, BigDecimal price);
 
     List<Beer> findAllByBeerNameIsLikeIgnoreCaseAndUpcIsLikeIgnoreCaseAndBeerStyleAndPriceGreaterThanAndPriceLessThan(String beerName, String upc, BeerStyle beerStyle, BigDecimal price, BigDecimal price2);
